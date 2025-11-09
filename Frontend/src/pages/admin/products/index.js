7f99a5be24883d22aa5f061fa964b0195fc8ea92
Rene Ixcopal Home <juan.ixcopal@alumnos.uneatlantico.es>
2025-11-09 08:06:50 +0100

    Removal of search engine due to lack of time to implement it

--- Frontend/src/pages/admin/products/index.js
+++ Frontend/src/pages/admin/products/index.js
@@ -1,6 +1,5 @@
-import { Box, Grid, Card, Typography, styled, InputBase, Button, IconButton } from '@mui/material'
+import { Box, Grid, Card, Typography, Button, IconButton } from '@mui/material'
 import { useFechtInitProducts } from '../../../hooks/products'
-import SearchIcon from '@mui/icons-material/Search'
 import EditSquareIcon from '@mui/icons-material/EditSquare'
 import DeleteIcon from '@mui/icons-material/Delete'
 import MainModal from './modal/index.js'
@@ -10,56 +9,18 @@ const ProductsPage = () => {
   const { fechtAllProducts, toggle } = mainHook
   const { allProducts } = fechtAllProducts
 
-  const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: '15px',
-    backgroundColor: '#FFFFFF',
-    width: '100%',
-    maxWidth: '400px'
-  }))
-
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: '#7B2D26'
-  }))
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: '#3E2F2F',
-    width: '100%',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1.5, 1.5, 1.5, 0),
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      fontSize: '15px'
-    }
-  }))
-
   return (
     <>
       <MainModal useFetchInit={mainHook} />
       <Grid container spacing={2} sx={{ marginBottom: '30px', alignItems: 'center' }}>
-        <Grid size={{ lg: 4, md: 12, sm: 12, xs: 12 }}>
+        <Grid size={{ lg: 6, md: 12, sm: 12, xs: 12 }}>
           <Typography fontSize={22} fontWeight={500} color='#3E2F2F'>
             Productos
           </Typography>
         </Grid>
 
-        <Grid size={{ lg: 4, md: 12, sm: 12, xs: 12 }}>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase placeholder='Buscar' inputProps={{ 'aria-label': 'search' }} />
-          </Search>
-        </Grid>
-
         <Grid
-          size={{ lg: 4, md: 6, sm: 12, xs: 12 }}
+          size={{ lg: 6, md: 6, sm: 12, xs: 12 }}
           sx={{ display: 'flex', justifyContent: 'flex-end' }}
         >
           <Button variant='contained' onClick={() => toggle(null, 'Crear producto', 'new-product')}>
